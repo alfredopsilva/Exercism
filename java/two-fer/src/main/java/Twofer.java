f57f2768c90fbd87544dedfcf01dfdb710d4494b
Alfredo Parreira <a.alfredops@gmail.com>
2023-01-09 21:20:42 -0500

    Exercism - Monday - 01/09/2023

---
+++ java/two-fer/src/main/java/Twofer.java
@@ -0,0 +1,11 @@
+import java.util.Objects;
+
+public class Twofer {
+    public String twofer(String name) {
+
+       if(name == null)
+           return "One for you, one for me.";
+       else
+           return "One for "+name+", one for me.";
+    }
+}
